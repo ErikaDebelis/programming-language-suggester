a1d52a21469396aee613e07c37dbf24bc521e3e2
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-04 14:00:24 -0700

    WIP: Trying to fix js to submit answers after pressing the button

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,23 @@ $(document).ready(function() {
     const vacation = $("input:radio[name=vacation]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const entertainment = $("input:radio[name=entertainment]:checked").val();
-
+    
     //possible answer combos to determine result/ language suggestion below// 
     // 4 if/if else statements for each possible language result//
 
-  if (spend === music && color === "p-color" && vacation === "tokyo" && entertainment === "games" || spend === "music" && color === "j-color" && vacation === "tokyo" && entertainment === "games" || spend === "food" && color === "p-color" && vacation === "tokyo" && entertainment === "games" || spend === "music" && color === "p-color" && vacation === "tokyo" && entertainment === "podcast") {
-    $('#csharp').show();
-  } else if (spend === "clothes" && color === "r-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv" || spend === "clothes" && color === "r-color" && vacation === "bora" && animal === "leopard" && entertainment === "tv" || spend === "food" && color === "r-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv" || spend === "clothes" && color === "j-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv" || spend === "clothes" && color === "r-color" && vacation === "paris" && animal === "meerkat" && entertainment === "tv") {
-    $('#ruby').show();
-  } else if (spend === "food" && color === "c-color" && vacation === "home" && animal === "rabbit" && entertainment === "podcast" || spend === "food" && color === "c-color" && vacation === "tokyo" && animal === "rabbit" && entertainment === "podcast" || spend === "food" && color === "c-color" && vacation === "home" && animal === "dog" && entertainment === "podcast" || spend === "food" && color === "c-color" && vacation === "home" && animal === "rabbit" && entertainment === "youtube" || spend === "food" && color === "j-color" && vacation === "home" && animal === "rabbit" && entertainment === "podcast") {
-    $('#js').show();
-  } else if (spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "meerkat" && entertainment === "tv" || spend === "gambling" && color === "j-color" && vacation === "paris" && animal === "meerkat" && entertainment === "tv" || spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "dog" && entertainment === "tv" || spend === "gambling" && color === "c-color" && vacation === "bora" && animal === "meerkat" && entertainment === "games" || spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "meerkat" && entertainment === "tv") {
-    $('#python').show();
-  }
+    if ((spend === "music" && color === "p-color" && vacation === "tokyo" && entertainment === "games") || (spend === "music" && color === "j-color" && vacation === "tokyo" && entertainment === "games") || (spend === "food" && color === "p-color" && vacation === "tokyo" && entertainment === "games") || (spend === "music" && color === "p-color" && vacation === "tokyo" && entertainment === "podcast")) {
+      $('#language').show();
+      $('.code-lang').text("C#").show();
+    } else if ((spend === "clothes" && color === "r-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv") || (spend === "clothes" && color === "r-color" && vacation === "bora" && animal === "leopard" && entertainment === "tv") || (spend === "food" && color === "r-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv") || (spend === "clothes" && color === "j-color" && vacation === "paris" && animal === "leopard" && entertainment === "tv") || (spend === "clothes" && color === "r-color" && vacation === "paris" && animal === "meerkat" && entertainment === "tv")) {
+      $('#language').show();
+      $('.output').text("Ruby").show();
+    } else if ((spend === "food" && color === "c-color" && vacation === "home" && animal === "rabbit" && entertainment === "podcast") || (spend === "food" && color === "c-color" && vacation === "tokyo" && animal === "rabbit" && entertainment === "podcast") || (spend === "food" && color === "c-color" && vacation === "home" && animal === "dog" && entertainment === "podcast") || (spend === "food" && color === "c-color" && vacation === "home" && animal === "rabbit" && entertainment === "youtube") || (spend === "food" && color === "j-color" && vacation === "home" && animal === "rabbit" && entertainment === "podcast")) {
+      $('#language').show();
+      $('.code-lang').text("JavaScript").show();
+    } else if ((spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "meerkat" && entertainment === "tv") || (spend === "gambling" && color === "j-color" && vacation === "paris" && animal === "meerkat" && entertainment === "tv") || (spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "dog" && entertainment === "tv") || (spend === "gambling" && color === "c-color" && vacation === "bora" && animal === "meerkat" && entertainment === "games") || (spend === "gambling" && color === "j-color" && vacation === "bora" && animal === "meerkat" && entertainment === "tv")) {
+      $('#language').show();
+      $('.code-lang').text("Python").show();
+    }
   });
 });
 
