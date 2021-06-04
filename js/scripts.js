a6c655fd5c869fd1b36ad502f5e7eda9f8c1cf3b
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-04 10:20:33 -0700

    begin js code & add comments to help keep track of js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
-
-
-
 $(document).ready(function() {
+  $("#suggester").submit(function(event) {
+    //the submitted answers to each of the 5 questions below//
+    const spend = 
+    const color =
+    const vacation =
+    const animal =
+    const entertainment =
+
+    //possible answer combos to determine result/ language suggestion below// 
+    if (spend === music && )
+
+  }
+
+
+
+
+// each question, their div class, name, and values below//
+
+//q1 name spend || options1-4 || values: music clothes food gambling  //
+//q2 name color || options1-4 || values: c-color r-color j-color p-color //
+//q3 name vacation || options 1-4 || values: tokyo paris bora home//
+//q4 name animal || options 1-4 || value: meerkat leopard rabbit dog //
+//q5 name entertainment || options 1-4 || value: tv games youtube podcast //
