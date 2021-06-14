e453c6b03f5a16a741e9cf9727324720758edcda
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-14 07:52:10 -0700

    WIP: need to reset after submitting new answers

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,22 @@ $(document).ready(function() {
     const vacation = $("input:radio[name=vacation]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const entertainment = $("input:radio[name=entertainment]:checked").val();
-
+    
     if ((spend === "csharp" && color === "csharp" && vacation === "csharp" && animal === "csharp" && entertainment === "csharp") || (spend === "csharp" && color === "csharp" && vacation === "csharp" && animal === "csharp" && entertainment === "ruby") || (spend === "javascript" && color === "csharp" && vacation === "csharp" && animal === "csharp" && entertainment === "csharp") || (spend === "csharp" && color === "csharp" && vacation === "csharp" && animal === "python" && entertainment === "csharp")) {
       $('#csharp').show();
+      event.preventDefault();
     } else if ((spend === "ruby" && color === "ruby" && vacation === "ruby" && animal === "ruby" && entertainment === "ruby") || (spend === "ruby" && color === "ruby" && vacation === "ruby" && animal === "ruby" && entertainment === "csharp") || (spend === "ruby" && color === "ruby" && vacation === "ruby" && animal === "javascript" && entertainment === "ruby") || (spend === "ruby" && color === "ruby" && vacation === "python" && animal === "ruby" && entertainment === "ruby") || (spend === "ruby" && color === "javascript" && vacation === "ruby" && animal === "ruby" && entertainment === "ruby")) {
       $('#ruby').show();
+      event.preventDefault();
     } else if ((spend === "javascript" && color === "javascript" && vacation === "javascript" && animal === "javascript" && entertainment === "javascript") || (spend === "javascript" && color === "javascript" && vacation === "javascript" && animal === "javascript" && entertainment === "csharp") || (spend === "javascript" && color === "javascript" && vacation === "javascript" && animal === "ruby" && entertainment === "javascript") || (spend === "javascript" && color === "javascript" && vacation === "ruby" && animal === "javascript" && entertainment === "javascript") || (spend === "javascript" && color === "python" && vacation === "javascript" && animal === "javascript" && entertainment === "javascript")) {
       $('#javascript').show();
+      event.preventDefault();
     } else if ((spend === "python" && color === "python" && vacation === "python" && animal === "python" && entertainment === "python") || (spend === "python" && color === "python" && vacation === "python" && animal === "python" && entertainment === "csharp") || (spend === "python" && color === "python" && vacation === "python" && animal === "ruby" && entertainment === "python") || (spend === "python" && color === "python" && vacation === "javascript" && animal === "python" && entertainment === "python") || (spend === "python" && color === "csharp" && vacation === "python" && animal === "python" && entertainment === "python")) {
       $('#python').show();
+      event.preventDefault();
     } else {
       $('#error').show();
+      event.preventDefault();
     }
   });
 });
